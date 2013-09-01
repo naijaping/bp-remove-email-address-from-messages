cd2de68c55ff445bf68795e20607d649be5ca071
naijaping <abbeyprince02@gmail.com>
2013-09-01 17:38:37 +0100

    Update bp-remove-email-address-from-messages.php

--- bp-remove-email-address-from-messages.php
+++ bp-remove-email-address-from-messages.php
@@ -1,4 +1,17 @@
 <?php
+/*
+Plugin Name: Bp Remove Email Address From Messages
+Plugin URI: http://cityflavourmagazine.com
+Description: Stop members from sending email address to each other by removing it automatically when sent.
+Version: 1.0
+Requires at least: WordPress 3.0 / BuddyPress 1.3
+Tested up to: WordPress 3.6  / BuddyPress 1.8.1 
+License: GNU/GPL 2
+Author URI: http://cityflavourmagazine.com/
+Author:Prince Abiola Ogundipe
+*/
+
+
 /**
  *  Make sure BuddyPress is loaded
  */
